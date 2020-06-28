eca9696bfd0864e0d207a24061a1f41da35fac08
Sharon Moriel <shmoriel@gmail.com>
2020-06-28 16:25:41 +0100

    added subscribers model

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,22 @@
 class LandingController < ApplicationController
-    def landingpage
+  def landing
+    @subscriber = Subscriber.new
+  end
+
+
+  def add_subscriber
+    @subscriber = Subscriber.new(subscriber_params)
+    if @subscriber.save
+
+    else
+      render :landing
     end
+  end
+
+  private
+
+  # Only allow a list of trusted parameters through.
+  def subscriber_params
+    params.require(:subscriber).permit(:email)
+  end
 end
